e52084acacc4768e738522dcb7aecb7c8e2f6032
ChaituGadige <149795056+ChaituGadige@users.noreply.github.com>
2024-06-06 17:41:44 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "pearl" {
   location = "East US"
 }
 
-resource "azurerm_app_service_plan" "app_srv_plan" {
+resource "azurerm_service_plan" "app_srv_plan" {
   name                = "wbapp-srv-pln"
   location            = azurerm_resource_group.pearl.location
   resource_group_name = azurerm_resource_group.pearl.name

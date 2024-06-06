2c7cf7fb1e6aede7a4b8f8310d0071b313a4fe84
ChaituGadige <149795056+ChaituGadige@users.noreply.github.com>
2024-06-06 16:53:34 +0530

    Create main.tf

---
+++ terraform-files/main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_resource_group" "pearl" {
+  name     = "Nodejs-rg"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "app_srv_plan" {
+  name                = "wbapp-srv-pln"
+  location            = azurerm_resource_group.pearl.location
+  resource_group_name = azurerm_resource_group.pearl.name
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "appsrv" {
+  name                = "webapp7216"
+  location            = azurerm_resource_group.pearl.location
+  resource_group_name = azurerm_resource_group.pearl.name
+  app_service_plan_id = azurerm_app_service_plan.app_srv_plan.id
+
+  
+  site_config {
+    use_32_bit_worker_process = true
+    linux_fx_version = "node|12-lts"
+  }
+
+  app_settings = {
+    WEBSITE_NODE_DEFAULT_VERSION = "12.13.0"
+ }
+}
